5f53d533ab1c21e722e4067f73567cbe8b160d6c
Hunter Haufler <haufhun@gmail.com>
2021-02-21 19:36:37 -0600

    Added storage for images to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { API } from 'aws-amplify';
-
 import logo from './logo.svg';
+
 import {withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+import { API, Storage } from 'aws-amplify';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+
 
 const initialFormState = { name: '', description: '' }
 
@@ -19,21 +21,54 @@ function App() {
 
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+
+    await Promise.all(notesFromAPI.map(async note => {
+      if (note.image) {
+        const image = await Storage.get(note.image);
+        note.image = image;
+      }
+
+      return note;
+    }))
+
     setNotes(apiData.data.listNotes.items);
   }
 
   async function createNote() {
     if (!formData.name || !formData.description) return;
 
-    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    await API.graphql(
+      { query: createNoteMutation, variables: { input: formData } })
+    ;
+
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+
     setNotes([ ...notes, formData ]);
     setFormData(initialFormState);
   }
 
   async function deleteNote({ id }) {
     const newNotesArray = notes.filter(note => note.id !== id);
+
     setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+
+    await API.graphql(
+      { query: deleteNoteMutation, variables: { input: { id } }}
+    );
+  }
+
+  async function onChange(e) {
+    if (!e.target.files[0]) return;
+
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+
+    fetchNotes();
   }
 
   return (
@@ -49,6 +84,10 @@ function App() {
         placeholder="Note description"
         value={formData.description}
       />
+      <input
+        type="file"
+        onChange={onChange}
+      />
       <button onClick={createNote}>Create Note</button>
       <div style={{marginBottom: 30}}>
         {
@@ -56,6 +95,8 @@ function App() {
             <div key={note.id || note.name}>
               <h2>{note.name}</h2>
               <p>{note.description}</p>
+              { note.image && <img src={note.image} style={{width: 400}} /> }
+
               <button onClick={() => deleteNote(note)}>Delete Note</button>
             </div>
           ))
